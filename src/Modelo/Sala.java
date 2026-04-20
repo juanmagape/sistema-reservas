a5f3b41698f4c57c7a5d05e50d3d8738a04b1803
juanmagape <juanmagape.trb@gmail.com>
2026-04-20 10:29:05 +0200

    Add Sala + Start MenuVista

--- src/Modelo/Sala.java
+++ src/Modelo/Sala.java
@@ -1,4 +1,57 @@
 package Modelo;
 
 public class Sala {
+    private int idSala;
+    private String nombre;
+    private String tematica;
+    private int capacidad;
+    private double precio;
+
+    public Sala(int idSala, String nombre, String tematica, int capacidad, double precio) {
+        this.idSala = idSala;
+        this.nombre = nombre;
+        this.tematica = tematica;
+        this.capacidad = capacidad;
+        this.precio = precio;
+    }
+
+    public int getIdSala() {
+        return idSala;
+    }
+
+    public void setIdSala(int idSala) {
+        this.idSala = idSala;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTematica() {
+        return tematica;
+    }
+
+    public void setTematica(String tematica) {
+        this.tematica = tematica;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
--- src/Vista/MenuVista.java
+++ src/Vista/MenuVista.java
@@ -1,4 +1,39 @@
 package Vista;
+import java.util.Scanner;
 
 public class MenuVista {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void Menu() {
+        while (true) {
+            System.out.println("================================================");
+            System.out.println("=Sistema de reservas ScapeRoom - Menú principal=");
+            System.out.println("================================================");
+
+            System.out.println("\n[1] - Crear reserva");
+            System.out.println("[2] - Ver reservas");
+            System.out.println("[3] - Eliminar reserva");
+            System.out.println("[4] - Salir");
+
+            int opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Has seleccionado: Crear reserva");
+                    break;
+                case 2:
+                    System.out.println("Has seleccionado: Ver reservas");
+                    break;
+                case 3:
+                    System.out.println("Has seleccionado: Eliminar reserva");
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, selecciona una opción del menú.");
+            }
+        }
+    }
 }
